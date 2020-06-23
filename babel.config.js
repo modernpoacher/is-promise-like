282ba3016ef77f43c74f276c6b6cb41d64975ed4
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-23 17:30:11 +0100

    `install` `prepare`

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,51 @@
-module.exports = {
-  compact: true,
-  comments: false,
-  presets: [
-    [
-      '@babel/env', {
-        targets: {
-          node: 'current'
-        },
-        useBuiltIns: 'usage',
-        corejs: 3
-      }
-    ]
-  ],
-  plugins: [
-    [
-      'module-resolver', {
-        alias: {
-          '~': '.'
-        }
-      }
-    ]
+const debug = require('debug')
+
+const log = debug('is-promise-like')
+
+const {
+  env: {
+    DEBUG = 'is-promise-like',
+    NODE_ENV = 'development'
+  }
+} = process
+
+debug.enable(DEBUG)
+
+const presets = [
+  [
+    '@babel/env', {
+      targets: {
+        node: 'current'
+      },
+      useBuiltIns: 'usage',
+      corejs: 3
+    }
   ]
+]
+
+const plugins = [
+  [
+    'module-resolver', {
+      alias: {
+        '~': '.'
+      }
+    }
+  ]
+]
+
+function using () {
+  log({ NODE_ENV })
+
+  return NODE_ENV === 'production'
+}
+
+module.exports = (api) => {
+  if (api) api.cache.using(using)
+
+  return {
+    compact: true,
+    comments: false,
+    presets,
+    plugins
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+const debug = require('debug')
+
+const log = debug('is-promise-like')
+
+const {
+  env: {
+    DEBUG = 'is-promise-like'
+  }
+} = process
+
+debug.enable(DEBUG)
+
 export function isPromise (p) {
+  log('isPromise')
+
   return (p instanceof Promise)
 }
 
 export function isPromiseLike (p) {
+  log('isPromiseLike')
+
   if (isPromise(p)) return true
   const {
     then
